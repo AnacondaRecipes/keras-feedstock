e5a36d3435432f95e965d8ed021a487bc3a5882a
Udo-Peter Steyer <ous8292@gmail.com>
2024-05-16 10:16:07 -0500

    adding patch and adding lines to scripts

--- recipe/install_base.bat
+++ recipe/install_base.bat
@@ -1,2 +1,4 @@
+@REM silently remove eventual leftover config (will set the backend)
+del %HOMEPATH%/.keras/keras.json 2> nul
 
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# remove eventual leftover config (will set the backend)
+rm -f ~/.keras/keras.json
+
 python -m pip install -vv $PKG_NAME-$PKG_VERSION-py3-none-any.whl
