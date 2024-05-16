bb69748af268cf976ec0ea9f00bc46d6e4f4299d
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-05-16 19:04:02 +0200

    clean up

--- recipe/install_base.bat
+++
@@ -1,9 +0,0 @@
-
-echo %HOMEPATH%/.keras/keras.json
-more %HOMEPATH%/.keras/keras.json
-dir %HOMEPATH%/.keras/keras.json
-
-@REM silently remove eventual leftover config (will set the backend)
-del %HOMEPATH%/.keras/keras.json 2> nul
-
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
